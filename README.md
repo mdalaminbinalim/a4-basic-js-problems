fed9ae7c78ecba6b1986584273e0bd80a3458d95
mdalaminbinalim <alamin29107@gmail.com>
2025-01-06 19:56:12 +0600

    started the project

---
+++ README.md
@@ -0,0 +1,1 @@
+# a4-basic-js-problems
---
+++ problems.js
@@ -0,0 +1,22 @@
+function calculateTax(income, expenses) {
+    const profit = income - expenses;
+    if ((income >= 0 && income >= expenses) && (expenses >= 0)) {
+        const tax = profit * 0.2;
+        return tax;
+    }
+    else {
+        return "Invalid Input";
+    }
+}
+
+
+function sendNotification(email) {
+    const separated = email.split('@');
+    const username = separated[0];
+    const domainName = separated[1];
+    return username + " " + "sent you an email from" + " " + domainName;
+}
+
+console.log(sendNotification('alamin29107@yahoo.com'))
+
+
